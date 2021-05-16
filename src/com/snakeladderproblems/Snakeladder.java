03436b32e1abe041677cacbca9e78781db2384c8
amitranjan <amitranjanpradhan456@gmail.com>
2021-05-16 14:05:03 +0530

    [Amit] Added The Game Between Two Players

--- src/com/snakeladderproblems/Snakeladder.java
+++ src/com/snakeladderproblems/Snakeladder.java
@@ -6,43 +6,79 @@ public class Snakeladder {
 	public static final int destination = 100;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			int position=0,diceRoll=0;
+		int position1=0,diceRoll1=0,position2=0,diceRoll2=0;
 				System.out.println("Player 1 is at 0 position now you can roll the die");
 				
-				while(position != destination)
+				
+				while(position1 < destination && position2<destination)
 				{
-					diceRoll++;
+					diceRoll1++;
 						int randomCheck = (int) Math.floor(Math.random() * 10) % 6+1;
 						System.out.println("You got the die roll :"+ randomCheck);
 						int randomPlay = (int) (Math.random() * 10) % 3;
 						switch(randomPlay) {
 						case 0:
 							System.out.println("No play");
-							position+=0;
+							position1+=0;
 							break;
 						case 1:
 							System.out.println("Well done");
-							position += randomCheck;
+							position1 += randomCheck;
 							break;
 						case 2:
 							System.out.println("Snake");
-							position -= randomCheck;
+							position1 -= randomCheck;
 							break;
 						}
-						
-						if (position == 100) {
-				                break;
-				            } else if (position > 100) {
-				                position -= randomCheck;
-				                System.out.println("Not valid Dice Face. You are staying on same position.");
-				            } else if (position < 0) {
-				                position = source; // Starting from zero again.
-				                System.out.println("You came back to start.");
-				            } else {
-				                System.out.println("New Position: " + position);
-				            }
-					}
-					System.out.println("You Won... and your position is : "+position);
-					System.out.println("Number of times Dicerolls to win is: "+diceRoll);
+			            if (position1 == 100) {
+			                break;
+			            } else if (position1 > 100) {
+			                position1 -= randomCheck;
+			                System.out.println("Not valid Dice Face. You are staying on same position.");
+			            } else if (position1 < 0) {
+			                position1 = source; // Starting from zero again.
+			                System.out.println("You came back to start.");
+			            } else {
+			                System.out.println("New Position player1: " + position1);
+			            }
+				  
+						diceRoll2++;
+						int randomCheck1 = (int) Math.floor(Math.random() * 10) % 6+1;
+						int randomPlay1 = (int) (Math.random() * 10) % 3;
+						switch(randomPlay1) {
+						case 0:
+							System.out.println("No play");
+							position2+=0;
+							break;
+						case 1:
+							System.out.println("Well done");
+							position2 += randomCheck1;
+							break;
+						case 2:
+							System.out.println("Snake");
+							position2 -= randomCheck1;
+							break;
+						}
+			            if (position2 == 100) {
+			                break;
+			            } else if (position2 > 100) {
+			                position2 -= randomCheck1;
+			                System.out.println("Not valid Dice Face. You are staying on same position.");
+			            } else if (position2< 0) {
+			                position2 = source; // Starting from zero again.
+			                System.out.println("You came back to start.");
+			            } else {
+			                System.out.println("New Position player2: " + position2);
+			            }
+				  }
+				System.out.println("Player1's position is : "+position1);
+				System.out.println("Number of times Dicerolls to win is: "+diceRoll1);
+				System.out.println("Player2's position is : "+position2);
+				System.out.println("Number of times Dicerolls to win is: "+diceRoll2);
+				if (position1==100) {
+					System.out.println("Congratulations!! Player1 Won");
+				}else {
+					System.out.println("Congratulations!! Player2 Won");
+				}
+				}
 			}
-	}
