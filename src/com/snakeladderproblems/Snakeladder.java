fe3fbd5365f73e9d64f004cea3b13cdb82c490e5
amitranjan <amitranjanpradhan456@gmail.com>
2021-05-16 13:09:07 +0530

    [Amit] Added Random die roll

--- src/com/snakeladderproblems/Snakeladder.java
+++ src/com/snakeladderproblems/Snakeladder.java
@@ -6,5 +6,7 @@ public class Snakeladder {
 		// TODO Auto-generated method stub
 				int position=0;
 				System.out.println("Player 1 is at 0 position now you can roll the die");
+				int randomCheck = (int) Math.floor(Math.random() * 10) % 6+1;
+				System.out.println("You got the die roll :"+ randomCheck);
 	}
 }
