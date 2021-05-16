260657530927744a14c323642f7a78b72f27c04f
amitranjan <amitranjanpradhan456@gmail.com>
2021-05-16 13:00:40 +0530

    [Amit] Added Player Position

---
+++ src/com/snakeladderproblems/Snakeladder.java
@@ -0,0 +1,10 @@
+package com.snakeladderproblems;
+
+public class Snakeladder {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				int position=0;
+				System.out.println("Player 1 is at 0 position now you can roll the die");
+	}
+}
