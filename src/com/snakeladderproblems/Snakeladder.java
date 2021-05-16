eb33f41e58538fd9532c4bf4b86f8d6b56986b18
amitranjan <amitranjanpradhan456@gmail.com>
2021-05-16 13:15:53 +0530

    [Amit] Added Switch case for die roll

--- src/com/snakeladderproblems/Snakeladder.java
+++ src/com/snakeladderproblems/Snakeladder.java
@@ -4,9 +4,24 @@ public class Snakeladder {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-				int position=0;
+			int position=0;
 				System.out.println("Player 1 is at 0 position now you can roll the die");
-				int randomCheck = (int) Math.floor(Math.random() * 10) % 6+1;
+			int randomCheck = (int) Math.floor(Math.random() * 10) % 6+1;
 				System.out.println("You got the die roll :"+ randomCheck);
+			int randomPlay = (int) (Math.random() * 10) % 3;
+			switch(randomPlay) {
+				case 0:
+					System.out.println("No play");
+					position+=0;
+					break;
+				case 1:
+					System.out.println("Ladder");
+					position += randomCheck;
+					break;
+				case 2:
+					System.out.println("Snake");
+					position -= randomCheck;
+					break;
+				}
 	}
 }
