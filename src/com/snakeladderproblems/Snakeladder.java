f68b5944c1f080699287161798bd7d160e416f90
amitranjan <amitranjanpradhan456@gmail.com>
2021-05-16 13:30:41 +0530

    [Amit] Added Repeat Till Winning

--- src/com/snakeladderproblems/Snakeladder.java
+++ src/com/snakeladderproblems/Snakeladder.java
@@ -1,27 +1,46 @@
 package com.snakeladderproblems;
 
 public class Snakeladder {
-	
+
+	public static final int source = 0;
+	public static final int destination = 100;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			int position=0;
+			int position=0,diceRoll=0;
 				System.out.println("Player 1 is at 0 position now you can roll the die");
-			int randomCheck = (int) Math.floor(Math.random() * 10) % 6+1;
-				System.out.println("You got the die roll :"+ randomCheck);
-			int randomPlay = (int) (Math.random() * 10) % 3;
-			switch(randomPlay) {
-				case 0:
-					System.out.println("No play");
-					position+=0;
-					break;
-				case 1:
-					System.out.println("Ladder");
-					position += randomCheck;
-					break;
-				case 2:
-					System.out.println("Snake");
-					position -= randomCheck;
-					break;
-				}
-	}
-}
+				
+				while(position != destination)
+				{
+					diceRoll++;
+						int randomCheck = (int) Math.floor(Math.random() * 10) % 6+1;
+						System.out.println("You got the die roll :"+ randomCheck);
+						int randomPlay = (int) (Math.random() * 10) % 3;
+						switch(randomPlay) {
+						case 0:
+							System.out.println("No play");
+							position+=0;
+							break;
+						case 1:
+							System.out.println("Well done");
+							position += randomCheck;
+							break;
+						case 2:
+							System.out.println("Snake");
+							position -= randomCheck;
+							break;
+						}
+			            if (position == 100) {
+			                break;
+			            } else if (position > 100) {
+			                position -= randomCheck;
+			                System.out.println("Not valid Dice Face. You are staying on same position.");
+			            } else if (position < 0) {
+			                position = source; // Starting from zero again.
+			                System.out.println("You came back to start.");
+			            } else {
+			                System.out.println("New Position: " + position);
+			            }
+				  }
+				System.out.println("You won");
+			  }
+		  }
